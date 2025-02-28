d477cd5e0531dcbcb24cc56c1963afd85b618d7f
ahmadjon09 <youtobe83a61a@gmail.com>
2025-02-28 23:41:14 +0500

    added update team and changed some style

--- src/Axios.jsx
+++ src/Axios.jsx
@@ -5,10 +5,6 @@ const is_auth = Cookies.get('is_auth')
 
 const instance = axios.create({
   baseURL: 'https://flowers-server-ohej.onrender.com/',
-<<<<<<< HEAD
-=======
-  //baseURL: 'http://localhost:8080/',
->>>>>>> 2e5f448ef2ba9ec7cafc502a22f522c567b8979f
   headers: {
     Authorization: is_auth
   }
